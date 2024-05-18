49dd398f606aece2b1de211fc38bd09059618967
ritwik-69 <72665321+ritwik-69@users.noreply.github.com>
2024-05-18 12:37:57 +0530

    connecting to mongodb

---
+++ src/index.js
@@ -0,0 +1,43 @@
+
+import dotenv from "dotenv"
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './.env'
+})
+
+
+connectDB()
+
+
+
+// .then(() => {
+//     app.listen(process.env.PORT || 8000, () => {
+//         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+//     })
+// })
+// .catch((err) => {
+//     console.log("MONGO db connection failed !!! ", err);
+// })
+
+
+
+// (async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//         app.on("error",(err)=>{ 
+//             console.log("Err:",err);
+//             throw error;
+//         });
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+
+//     }
+//     catch (error) {
+//         console.log(error);
+//         throw error
+//     }
+
+// })
